62f6514124d14fb7da1ff62306a903585b2c9f6e
sayed9609 <sayedmahmud9609@gmail.com>
2023-11-09 22:21:46 +0600

    Assignment Done!

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/user',[UserController::class,"index"]);
+Route::get('/',[UserController::class,"index"]);
